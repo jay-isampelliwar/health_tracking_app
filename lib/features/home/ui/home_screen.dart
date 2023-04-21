6badb811fcf66c60febc6f9f5aca6a2a9e6c1e51
jay-isampelliwar <isampelliwar79@gmail.com>
2023-04-21 23:52:32 +0530

    .

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -1,56 +1,86 @@
 import 'package:flutter/material.dart';
-import 'package:health_tracking_app/core/constants/color_constant.dart';
+import 'package:health_tracking_app/core/constants/text_styles.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Hello World",
-              style: TextStyle(
-                color: AppColors.darkBlue,
-                fontSize: 55,
-                fontFamily: "GilroyBold",
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 100,
+        ),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Hello World",
+                style: AppTextStyles.text12(
+                  bold: false,
+                  size: size,
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Hello World",
-              style: TextStyle(
-                color: AppColors.lightOrange,
-                fontSize: 55,
-                fontFamily: "GilroyLight",
+              Text(
+                "Hello World",
+                style: AppTextStyles.text14(
+                  bold: false,
+                  size: size,
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Hello World",
-              style: TextStyle(
-                color: AppColors.grey,
-                fontSize: 55,
+              Text(
+                "Hello World",
+                style: AppTextStyles.text16(
+                  bold: false,
+                  size: size,
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Hello World",
-              style: TextStyle(
-                color: AppColors.lightGrey,
-                fontSize: 55,
+              Text(
+                "Hello World",
+                style: AppTextStyles.text18(
+                  bold: false,
+                  size: size,
+                ),
               ),
-            ),
-          ],
+              Text(
+                "Hello World",
+                style: AppTextStyles.text20(
+                  bold: false,
+                  size: size,
+                ),
+              ),
+              Text(
+                "Hello World",
+                style: AppTextStyles.text22(
+                  bold: false,
+                  size: size,
+                ),
+              ),
+              Text(
+                "Hello World",
+                style: AppTextStyles.text24(
+                  bold: false,
+                  size: size,
+                ),
+              ),
+              Text(
+                "Hello World",
+                style: AppTextStyles.text26(
+                  bold: false,
+                  size: size,
+                ),
+              ),
+              Text(
+                "Hello World",
+                style: AppTextStyles.text28(
+                  bold: false,
+                  size: size,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
