489846adf5c271fb066169b519d6ade05cc40132
jay-isampelliwar <isampelliwar79@gmail.com>
2023-04-21 19:41:30 +0530

    added Colors files

---
+++ lib/core/constants/color_constant.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class AppColors {
+  static Color lightGrey = HexColor("#E9E9EB");
+  static Color grey = HexColor("#C0BEC5");
+  static Color darkBlue = HexColor("#5142AB");
+  static Color lightOrange = HexColor("#FF8354");
+  static Color white = Colors.white;
+}
