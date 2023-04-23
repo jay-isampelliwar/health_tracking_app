47bd33686a5b9217736665418049ccff4dafc710
jay-isampelliwar <isampelliwar79@gmail.com>
2023-04-23 21:55:08 +0530

    added water color shades

--- lib/core/constants/color_constant.dart
+++ lib/core/constants/color_constant.dart
@@ -9,4 +9,6 @@ class AppColors {
   static Color white = Colors.white;
   static Color black = Colors.black;
   static Color heartBar = HexColor("#FC2947");
+  static Color waterColorShade1 = HexColor("#0870D7");
+  static Color waterColorShade2 = HexColor("#2394DF");
 }
