912cbe35c5ae9a943a4c515b7e6fc88bb6c54dbd
jay-isampelliwar <isampelliwar79@gmail.com>
2023-04-24 14:37:31 +0530

    removed curved_nav_bar

--- lib/core/widgets/app_bottom_navbar.dart
+++ lib/core/widgets/app_bottom_navbar.dart
@@ -1,21 +1,49 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:health_tracking_app/core/constants/color_constant.dart';
 
 List<Widget> item = [
-  Icon(Icons.home, color: AppColors.darkBlue),
-  Icon(Icons.home_repair_service_rounded, color: AppColors.darkBlue),
-  Icon(Icons.chat_outlined, color: AppColors.darkBlue),
-  Icon(Icons.graphic_eq_rounded, color: AppColors.darkBlue),
-  Icon(Icons.person, color: AppColors.darkBlue),
+  const NavBarItem(
+    path: "lib/assets/images/home.png",
+  ),
+  const NavBarItem(
+    path: "lib/assets/images/achievement.png",
+  ),
+  const NavBarItem(
+    path: "lib/assets/images/bot.png",
+  ),
+  const NavBarItem(
+    path: "lib/assets/images/stats.png",
+  ),
+  const NavBarItem(
+    path: "lib/assets/images/profile.png",
+  ),
 ];
 
-CurvedNavigationBar appNavigationBar(Size size) {
-  return CurvedNavigationBar(
-    items: item,
-    animationDuration: const Duration(milliseconds: 200),
+class NavBarItem extends StatelessWidget {
+  const NavBarItem({
+    required this.path,
+    Key? key,
+  }) : super(key: key);
+  final String path;
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SizedBox(
+      height: size.width * 0.08,
+      child: Image.asset(
+        path,
+        color: AppColors.darkBlue,
+      ),
+    );
+  }
+}
+
+NavigationBar appNavigationBar(Size size) {
+  return NavigationBar(
+    backgroundColor: AppColors.white,
+    selectedIndex: 1,
+    destinations: item,
+    animationDuration: const Duration(milliseconds: 300),
     height: size.height * 0.08,
-    backgroundColor: Colors.transparent,
-    color: AppColors.white,
   );
 }
