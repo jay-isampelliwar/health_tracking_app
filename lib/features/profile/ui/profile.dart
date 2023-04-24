f5640198066f66dcd7d9fa1cb55ec5b3c8b9163d
jay-isampelliwar <isampelliwar79@gmail.com>
2023-04-24 22:10:18 +0530

    added Ui

---
+++ lib/features/profile/ui/profile.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:health_tracking_app/core/constants/color_constant.dart';
+import 'package:health_tracking_app/core/constants/text_styles.dart';
+
+import '../../../core/widgets/app_custom_app_bar.dart';
+import '../../../core/widgets/const_size_box.dart';
+
+class Profile extends StatelessWidget {
+  const Profile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.only(
+          left: size.width * 0.06,
+          right: size.width * 0.06,
+          top: size.height * 0.06,
+          bottom: size.height * 0.04,
+        ),
+        child: Column(
+          children: [
+            CustomAppBar(
+              title: "Your Profile",
+              subtitle: "",
+            ),
+            AppConstSizeBox.constHightSizedBox(size.height * 0.04),
+            Column(
+              children: [
+                ProfileOptionContainer(
+                  title: "Edit Profile",
+                  icon: const Icon(Icons.edit),
+                  onTap: () {},
+                ),
+                ProfileOptionContainer(
+                  title: "Edit Goals",
+                  icon: const Icon(Icons.edit),
+                  onTap: () {},
+                ),
+                ProfileOptionContainer(
+                  title: "Share Data",
+                  icon: const Icon(Icons.share),
+                  onTap: () {},
+                ),
+                ProfileOptionContainer(
+                  title: "Theme",
+                  icon: const Icon(Icons.color_lens),
+                  onTap: () {},
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileOptionContainer extends StatelessWidget {
+  ProfileOptionContainer({
+    required this.icon,
+    required this.title,
+    required this.onTap,
+    Key? key,
+  }) : super(key: key);
+
+  final Icon icon;
+  final String title;
+  Function()? onTap;
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: size.height * 0.07,
+        padding: EdgeInsets.symmetric(horizontal: size.width * 0.08),
+        margin: EdgeInsets.only(bottom: size.height * 0.01),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(
+              size.width * 0.02,
+            ),
+            border: Border.all(color: AppColors.lightGrey, width: 1)),
+        child: Row(
+          children: [
+            Row(
+              children: [
+                Text(
+                  title,
+                  style: AppTextStyles.text22(bold: true, size: size),
+                ),
+                AppConstSizeBox.constWidthSizedBox(size.width * 0.03),
+                icon
+              ],
+            ),
+            const Spacer(),
+            const Icon(Icons.arrow_forward_ios_rounded)
+          ],
+        ),
+      ),
+    );
+  }
+}
