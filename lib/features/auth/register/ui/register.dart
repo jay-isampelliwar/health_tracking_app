4278d002214d405611a45418b5c155388e052229
jay-isampelliwar <isampelliwar79@gmail.com>
2023-04-25 17:59:11 +0530

    added image picker

--- lib/features/auth/register/ui/register.dart
+++ lib/features/auth/register/ui/register.dart
@@ -1,6 +1,10 @@
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:health_tracking_app/core/widgets/app_custom_app_bar.dart';
 import 'package:health_tracking_app/features/auth/login/ui/login.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../../../../core/constants/color_constant.dart';
 import '../../../../core/constants/text_styles.dart';
@@ -21,13 +25,26 @@ class _RegistrationState extends State<Registration> {
   TextEditingController nameTextEditingController = TextEditingController();
   TextEditingController passwordTextEditingController = TextEditingController();
   GlobalKey<FormState> formKey = GlobalKey<FormState>();
-
+  File? image;
   @override
   void dispose() {
     emailTextEditingController.dispose();
     nameTextEditingController.dispose();
     passwordTextEditingController.dispose();
     super.dispose();
+  }
+
+  Future pickImage() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      if (image == null) return;
+      final tempImage = File(image.path);
+      setState(() {
+        this.image = tempImage;
+      });
+    } catch (err) {
+      log(err.toString());
+    }
   }
 
   @override
@@ -62,32 +79,39 @@ class _RegistrationState extends State<Registration> {
                   children: [
                     GestureDetector(
                       onTap: () {
-                        //! Upload image
+                        pickImage();
                       },
                       child: Container(
-                        width: size.width * 0.25,
-                        height: size.width * 0.25,
-                        decoration: BoxDecoration(
-                          border:
-                              Border.all(color: AppColors.darkBlue, width: 2),
-                          shape: BoxShape.circle,
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(
-                              Icons.upload,
-                              size: size.width * 0.08,
-                              color: AppColors.darkBlue,
-                            ),
-                            Text(
-                              "Upload Profile",
-                              style:
-                                  AppTextStyles.text12(bold: false, size: size),
-                            )
-                          ],
-                        ),
-                      ),
+                          clipBehavior: Clip.hardEdge,
+                          width: size.width * 0.25,
+                          height: size.width * 0.25,
+                          decoration: BoxDecoration(
+                            border: image != null
+                                ? null
+                                : Border.all(
+                                    color: AppColors.darkBlue, width: 2),
+                            shape: BoxShape.circle,
+                          ),
+                          child: image == null
+                              ? Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(
+                                      Icons.upload,
+                                      size: size.width * 0.08,
+                                      color: AppColors.darkBlue,
+                                    ),
+                                    Text(
+                                      "Upload Profile",
+                                      style: AppTextStyles.text12(
+                                          bold: false, size: size),
+                                    )
+                                  ],
+                                )
+                              : Image.file(
+                                  image!,
+                                  fit: BoxFit.cover,
+                                )),
                     ),
                     AppConstSizeBox.constHightSizedBox(size.height * 0.01),
                     AppTextField(
@@ -117,9 +141,13 @@ class _RegistrationState extends State<Registration> {
                       title: "Register",
                       onTap: () {
                         FocusScope.of(context).unfocus();
-                        // if (formKey.currentState!.validate()) {
-                        //! Navigate to homepage
-                        // }
+                        if (formKey.currentState!.validate()) {
+                          //! Navigate to homepage
+                        }
+
+                        nameTextEditingController.clear();
+                        emailTextEditingController.clear();
+                        passwordTextEditingController.clear();
                       },
                     ),
                     AppConstSizeBox.constHightSizedBox(size.height * 0.02),
