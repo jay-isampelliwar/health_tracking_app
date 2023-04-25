1fe1dd94a783b299ec83add44eb6f6125e002f62
jay-isampelliwar <isampelliwar79@gmail.com>
2023-04-25 15:11:13 +0530

    added email name pass validation methods

--- lib/core/helper/helper.dart
+++ lib/core/helper/helper.dart
@@ -10,4 +10,69 @@ class Helper {
       return 'Good evening';
     }
   }
+
+  static String? emailValidator(String? val) {
+    if (val!.isEmpty) return "Required";
+    RegExp emailRegex = RegExp(
+        r"^[a-zA-Z0-9.!#$%&\'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$");
+
+    return emailRegex.hasMatch(val) ? null : "Invalid Email!";
+  }
+
+  static String? passwordValidator(String? password) {
+    if (password!.isEmpty) return "Required";
+    if (password.length < 8) {
+      return "Required 8 or more than 8 letter password";
+    }
+
+    // Check if password contains at least one uppercase letter
+    if (password.toUpperCase() == password) {
+      return "At least one lower case character required";
+    }
+
+    // Check if password contains at least one lowercase letter
+    if (password.toLowerCase() == password) {
+      return "At least one upper case character required";
+    }
+
+    // Check if password contains at least one digit
+    if (!password.contains(RegExp(r'\d'))) {
+      return "At least one digit required";
+    }
+
+    // Check if password contains at least one special character
+    if (!password.contains(RegExp(r'[!@#\$%\^&\*()_\+\-\={}<>\?,\.\/~`|\\]'))) {
+      return "At least one special character required";
+    }
+
+    return null;
+  }
+
+  static String? numberValidator(String? val) {
+    if (val!.isEmpty) return "Required";
+    RegExp phoneRegex = RegExp(r'^[6-9]\d{9}$');
+
+    return phoneRegex.hasMatch(val) ? null : "Please enter valid number";
+  }
+
+  static String? nameValidator(String? name) {
+    if (name!.isEmpty) return "Required";
+    if (name.split(" ").length == 1) return "Enter Name and Surname";
+    RegExp nameRegex = RegExp(r'^[a-zA-Z\s]+$');
+
+    return nameRegex.hasMatch(name) ? null : "Invalid Name";
+  }
+
+  static String? otpValidator(String? val) {
+    if (val!.isEmpty) return "Required";
+    if (val.length != 6) {
+      return "OTP Must be 6 Digit number";
+    }
+
+    final regex = RegExp(r'^[0-9]+$');
+    if (!regex.hasMatch(val)) {
+      return "Please Enter valid OTP";
+    }
+    return null;
+  }
 }
