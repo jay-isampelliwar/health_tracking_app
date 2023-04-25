2edf9972127d426115f1f800b7474c2bf63cce64
jay-isampelliwar <isampelliwar79@gmail.com>
2023-04-25 15:11:18 +0530

    added

---
+++ lib/features/goal/ui/goal.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Goal extends StatelessWidget {
+  const Goal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
