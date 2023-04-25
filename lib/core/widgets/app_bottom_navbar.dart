6ca3797d74ebc052e863bef4acd710f5bd72ce2c
jay-isampelliwar <isampelliwar79@gmail.com>
2023-04-25 10:38:04 +0530

    added gnav bar

--- lib/core/widgets/app_bottom_navbar.dart
+++ lib/core/widgets/app_bottom_navbar.dart
@@ -2,81 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:health_tracking_app/core/constants/color_constant.dart';
 
-List<Widget> item = [
-  const NavBarItem(
-    path: "lib/assets/images/home.png",
-  ),
-  const NavBarItem(
-    path: "lib/assets/images/achievement.png",
-  ),
-  const NavBarItem(
-    path: "lib/assets/images/bot.png",
-  ),
-  const NavBarItem(
-    path: "lib/assets/images/stats.png",
-  ),
-  const NavBarItem(
-    path: "lib/assets/images/profile.png",
-  ),
-];
-
-class NavBarItem extends StatelessWidget {
-  const NavBarItem({
-    required this.path,
-    Key? key,
-  }) : super(key: key);
-  final String path;
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return SizedBox(
-      height: size.width * 0.08,
-      child: Image.asset(
-        path,
-        color: AppColors.darkBlue,
+Container appNavigationBar(
+    {required Size size, required Function(int) onTabChange}) {
+  return Container(
+    color: Colors.transparent,
+    child: Padding(
+      padding: EdgeInsets.symmetric(
+          vertical: size.height * 0.01, horizontal: size.width * 0.03),
+      child: GNav(
+        activeColor: AppColors.white,
+        color: AppColors.black,
+        duration: const Duration(milliseconds: 300),
+        tabBackgroundColor: AppColors.darkBlue,
+        tabActiveBorder: Border.all(color: AppColors.darkBlue, width: 1),
+        onTabChange: onTabChange,
+        tabs: [
+          GButton(
+            icon: Icons.home_outlined,
+            padding: EdgeInsets.all(size.width * 0.03),
+          ),
+          GButton(
+            icon: Icons.wallet_giftcard_outlined,
+            padding: EdgeInsets.all(size.width * 0.03),
+          ),
+          GButton(
+            icon: Icons.graphic_eq_rounded,
+            padding: EdgeInsets.all(size.width * 0.03),
+          ),
+          GButton(
+            icon: Icons.person_outline,
+            padding: EdgeInsets.all(size.width * 0.03),
+          ),
+          GButton(
+            icon: Icons.chat_outlined,
+            padding: EdgeInsets.all(size.width * 0.03),
+          ),
+        ],
       ),
-    );
-  }
-}
-
-GNav appNavigationBar(Size size) {
-  return GNav(
-      rippleColor: Colors.grey.shade800, // tab button ripple color when pressed
-      hoverColor: Colors.grey.shade700, // tab button hover color
-      haptic: true, // haptic feedback
-      tabBorderRadius: 15,
-      tabActiveBorder:
-          Border.all(color: Colors.black, width: 1), // tab button border
-      tabBorder: Border.all(color: Colors.grey, width: 1), // tab button border
-      tabShadow: [
-        BoxShadow(color: Colors.grey.withOpacity(0.5), blurRadius: 8)
-      ], // tab button shadow
-      curve: Curves.easeOutExpo, // tab animation curves
-      duration: const Duration(milliseconds: 900), // tab animation duration
-      gap: 8, // the tab button gap between icon and text
-      color: Colors.grey[800], // unselected icon color
-      activeColor: Colors.purple, // selected icon and text color
-      iconSize: 24, // tab button icon size
-      tabBackgroundColor:
-          Colors.purple.withOpacity(0.1), // selected tab background color
-      padding: const EdgeInsets.symmetric(
-          horizontal: 20, vertical: 5), // navigation bar padding
-      tabs: const [
-        GButton(
-          icon: Icons.home,
-          text: 'Home',
-        ),
-        GButton(
-          icon: Icons.favorite,
-          text: 'Likes',
-        ),
-        GButton(
-          icon: Icons.search,
-          text: 'Search',
-        ),
-        GButton(
-          icon: Icons.person,
-          text: 'Profile',
-        )
-      ]);
+    ),
+  );
 }
