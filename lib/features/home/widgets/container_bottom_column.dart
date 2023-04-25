bfb334d599b7f373059269352ecedf10aeb08e9e
jay-isampelliwar <isampelliwar79@gmail.com>
2023-04-25 10:39:16 +0530

    added in separate file

---
+++ lib/features/home/widgets/container_bottom_column.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/constants/color_constant.dart';
+import '../../../core/constants/text_styles.dart';
+
+class ContainerBottomColumn extends StatelessWidget {
+  const ContainerBottomColumn({
+    Key? key,
+    required this.value,
+    required this.title,
+    required this.color,
+  }) : super(key: key);
+
+  final String value;
+  final String title;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          value,
+          style: AppTextStyles.text18(bold: true, size: size)
+              .copyWith(color: color),
+        ),
+        Text(
+          title,
+          style: AppTextStyles.text18(bold: true, size: size)
+              .copyWith(color: AppColors.grey),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/features/home/widgets/step_progress_indicator.dart
@@ -0,0 +1,40 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+import '../../../core/constants/color_constant.dart';
+
+class StepProgressIndicator extends CustomPainter {
+  int step;
+  StepProgressIndicator({required this.step});
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint circle = Paint()
+      ..strokeWidth = ((size.width + size.height) / 2) * 0.3
+      ..color = AppColors.grey.withOpacity(0.2)
+      ..style = PaintingStyle.stroke;
+
+    Offset offset = Offset(size.width / 2, size.height / 2);
+    canvas.drawCircle(offset, ((size.width + size.height) / 2) * 1.3, circle);
+
+    Paint arc = Paint()
+      ..strokeWidth = ((size.width + size.height) / 2) * 0.3
+      ..color = AppColors.white
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round;
+
+    double angle = 2 * pi * (step / 10000);
+    canvas.drawArc(
+        Rect.fromCircle(
+            center: offset, radius: ((size.width + size.height) / 2) * 1.3),
+        4.8,
+        angle,
+        false,
+        arc);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
