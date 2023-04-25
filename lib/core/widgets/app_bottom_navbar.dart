7fa704fa4e7b6f0dfed1488cd9bb2b238fb82a99
jay-isampelliwar <isampelliwar79@gmail.com>
2023-04-25 09:32:22 +0530

    added google_nav_bar pkg

--- lib/core/widgets/app_bottom_navbar.dart
+++ lib/core/widgets/app_bottom_navbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:health_tracking_app/core/constants/color_constant.dart';
 
 List<Widget> item = [
@@ -38,12 +39,44 @@ class NavBarItem extends StatelessWidget {
   }
 }
 
-NavigationBar appNavigationBar(Size size) {
-  return NavigationBar(
-    backgroundColor: AppColors.white,
-    selectedIndex: 1,
-    destinations: item,
-    animationDuration: const Duration(milliseconds: 300),
-    height: size.height * 0.08,
-  );
+GNav appNavigationBar(Size size) {
+  return GNav(
+      rippleColor: Colors.grey.shade800, // tab button ripple color when pressed
+      hoverColor: Colors.grey.shade700, // tab button hover color
+      haptic: true, // haptic feedback
+      tabBorderRadius: 15,
+      tabActiveBorder:
+          Border.all(color: Colors.black, width: 1), // tab button border
+      tabBorder: Border.all(color: Colors.grey, width: 1), // tab button border
+      tabShadow: [
+        BoxShadow(color: Colors.grey.withOpacity(0.5), blurRadius: 8)
+      ], // tab button shadow
+      curve: Curves.easeOutExpo, // tab animation curves
+      duration: const Duration(milliseconds: 900), // tab animation duration
+      gap: 8, // the tab button gap between icon and text
+      color: Colors.grey[800], // unselected icon color
+      activeColor: Colors.purple, // selected icon and text color
+      iconSize: 24, // tab button icon size
+      tabBackgroundColor:
+          Colors.purple.withOpacity(0.1), // selected tab background color
+      padding: const EdgeInsets.symmetric(
+          horizontal: 20, vertical: 5), // navigation bar padding
+      tabs: const [
+        GButton(
+          icon: Icons.home,
+          text: 'Home',
+        ),
+        GButton(
+          icon: Icons.favorite,
+          text: 'Likes',
+        ),
+        GButton(
+          icon: Icons.search,
+          text: 'Search',
+        ),
+        GButton(
+          icon: Icons.person,
+          text: 'Profile',
+        )
+      ]);
 }
