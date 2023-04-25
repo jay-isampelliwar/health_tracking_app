194afc04c1406943166e62dc24e0b1ad557b9a5f
jay-isampelliwar <isampelliwar79@gmail.com>
2023-04-25 10:37:52 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      // home: HomePage(),
+      home: MainWidget(),
       // home: Stats(),
       // home: Profile(),
       // home: Achievement(),
