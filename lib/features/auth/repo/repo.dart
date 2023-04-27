e773aefe1ba638c339d86d88a4baa1aeaa187109
jay-isampelliwar <isampelliwar79@gmail.com>
2023-04-27 20:00:38 +0530

    change return type void to model

--- lib/features/auth/repo/repo.dart
+++ lib/features/auth/repo/repo.dart
@@ -1,19 +1,20 @@
+import 'package:health_tracking_app/features/auth/model/auth_model.dart';
 import 'package:health_tracking_app/features/auth/model/user_model.dart';
 
 import '../../../api_provider/api_provider.dart';
 
-class AchievementRepo {
+class AuthRepo {
   final ApiProvider _apiProvider = ApiProvider();
 
-  void login(User model) {
-    _apiProvider.login(model);
+  Future<AuthModel> login(User model) {
+    return _apiProvider.login(model);
   }
 
-  void registration(User model) {
-    _apiProvider.registration(model);
+  Future<AuthModel> registration(User model) {
+    return _apiProvider.registration(model);
   }
 
-  void verifyOtp(User model) {
-    _apiProvider.verifyOtp();
+  Future<AuthModel> verifyOtp(User model) {
+    return _apiProvider.verifyOtp();
   }
 }
