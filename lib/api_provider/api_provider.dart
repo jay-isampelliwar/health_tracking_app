add438b7dffcbec4a6c70c15ae8355a17e452bf8
jay-isampelliwar <isampelliwar79@gmail.com>
2023-04-27 17:33:50 +0530

    modify model

--- lib/api_provider/api_provider.dart
+++ lib/api_provider/api_provider.dart
@@ -1,3 +1,5 @@
+import 'package:health_tracking_app/core/constants/app_url.dart';
+import 'package:health_tracking_app/features/auth/model/auth_model.dart';
 import 'package:health_tracking_app/features/auth/model/user_model.dart';
 import 'package:health_tracking_app/features/home/model/user_data.dart';
 import 'package:http/http.dart' as http;
@@ -8,16 +10,43 @@ class ApiProvider {
   static final _client = http.Client();
   http.Client get client => _client;
 
-  Future<void> login(User? model) async {
-    try {} catch (err) {}
+  Future<AuthModel> login(User? model) async {
+    try {
+      var body = {"email": model!.email, "password": model.password};
+
+      var uri = Uri.parse(BASE_URL + USER_LOGIN);
+      var response = await client.post(uri, body: body);
+      return authModelFromJson(response.body);
+    } catch (err) {
+      return AuthModel(status: false, message: err.toString(), data: null);
+    }
   }
 
-  Future<void> registration(User? model) async {
-    try {} catch (err) {}
+  Future<AuthModel> registration(User? model) async {
+    try {
+      var body = {
+        "name": model!.name,
+        "email": model.email,
+        "password": model.password
+      };
+
+      var uri = Uri.parse(BASE_URL + USER_REGISTRATION);
+      var response = await client.post(uri, body: body);
+      return authModelFromJson(response.body);
+    } catch (err) {
+      return AuthModel(status: false, message: err.toString(), data: null);
+    }
   }
 
-  Future<void> verifyOtp({User? model}) async {
-    try {} catch (err) {}
+  Future<AuthModel> verifyOtp({User? model}) async {
+    try {
+      var body = {"otp": model!.otp, "email": model.email};
+      var uri = Uri.parse(BASE_URL + USER_VERIFY_OTP);
+      var response = await client.post(uri, body: body);
+      return authModelFromJson(response.body);
+    } catch (err) {
+      return AuthModel(status: false, message: err.toString(), data: null);
+    }
   }
 
   Future<User> userDetails() async {
