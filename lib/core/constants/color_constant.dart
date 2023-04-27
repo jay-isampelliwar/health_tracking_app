74c102a00fb93a5ca564a7bfebb467ffd46202f8
jay-isampelliwar <isampelliwar79@gmail.com>
2023-04-27 19:58:58 +0530

    theme change

--- lib/core/constants/color_constant.dart
+++ lib/core/constants/color_constant.dart
@@ -2,16 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 class AppColors {
-  // static Color lightSecondaryColor = HexColor("#E9E9EB");
-  // static Color secondaryColor = HexColor("#C0BEC5");
-  // static Color primaryColor = HexColor("#5142AB");
-  // static Color white = Colors.white;
-  // static Color black = Colors.black;
-  static Color lightSecondaryColor = HexColor("#A5D7E8");
-  static Color secondaryColor = HexColor("19376D");
-  static Color primaryColor = HexColor("#0B2447");
+  static Color lightSecondaryColor = HexColor("#E9E9EB");
+  static Color secondaryColor = HexColor("#C0BEC5");
+  static Color primaryColor = HexColor("#5142AB");
   static Color white = Colors.white;
   static Color black = Colors.black;
+  // static Color lightSecondaryColor = HexColor("#A5D7E8");
+  // static Color secondaryColor = HexColor("19376D");
+  // static Color primaryColor = HexColor("#0B2447");
+  // static Color white = Colors.white;
+  // static Color black = Colors.black;
 
   static Color heartBar = HexColor("#FC2947");
   static Color waterColorShade1 = HexColor("#0870D7");
