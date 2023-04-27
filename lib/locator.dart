65fd159981e9ec67be75426e5e6e81ad38be6323
jay-isampelliwar <isampelliwar79@gmail.com>
2023-04-27 19:59:42 +0530

    added intances

--- lib/locator.dart
+++ lib/locator.dart
@@ -2,11 +2,12 @@ import 'package:get_it/get_it.dart';
 import 'package:health_tracking_app/features/achievement/bloc/achievement_bloc.dart';
 import 'package:health_tracking_app/features/auth/login/bloc/login_bloc.dart';
 import 'package:health_tracking_app/features/auth/register/bloc/sign_up_bloc.dart';
+import 'package:health_tracking_app/features/auth/repo/repo.dart';
 import 'package:health_tracking_app/features/goal/bloc/goal_bloc.dart';
 import 'package:health_tracking_app/features/home/bloc/home_bloc.dart';
 import 'package:health_tracking_app/features/stats/bloc/stats_bloc.dart';
 
-final locator = GetIt.instance;
+GetIt locator = GetIt.instance;
 void setup() {
   locator.registerLazySingleton<HomeBloc>(() => HomeBloc());
   locator.registerLazySingleton<LoginBloc>(() => LoginBloc());
@@ -14,4 +15,6 @@ void setup() {
   locator.registerLazySingleton<AchievementBloc>(() => AchievementBloc());
   locator.registerLazySingleton<StatsBloc>(() => StatsBloc());
   locator.registerLazySingleton<GoalBloc>(() => GoalBloc());
+
+  locator.registerLazySingleton<AuthRepo>(() => AuthRepo());
 }
