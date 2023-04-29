9c679e046598ee60f8cf2c9beb8be2576f111f3f
jay-isampelliwar <isampelliwar79@gmail.com>
2023-04-29 20:11:38 +0530

    added dynamic water container

--- lib/core/widgets/app_water_container.dart
+++ lib/core/widgets/app_water_container.dart
@@ -11,62 +11,67 @@ class WaterContainer extends StatelessWidget {
   WaterContainer({
     required this.val1,
     required this.val2,
+    this.onTap,
     Key? key,
   }) : super(key: key);
 
   double val1;
   double val2;
+  Function()? onTap;
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Expanded(
-      child: Container(
-        clipBehavior: Clip.hardEdge,
-        // margin: EdgeInsets.symmetric(horizontal: size.width * 0.01),
-        decoration: BoxDecoration(
-          border: Border.all(width: 1, color: AppColors.secondaryColor),
-          borderRadius: BorderRadius.circular(size.width * 0.07),
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          clipBehavior: Clip.hardEdge,
+          // margin: EdgeInsets.symmetric(horizontal: size.width * 0.01),
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: AppColors.secondaryColor),
+            borderRadius: BorderRadius.circular(size.width * 0.07),
+          ),
+          child: Stack(children: [
+            WaveWidget(
+              config: CustomConfig(
+                colors: [
+                  AppColors.waterColorShade1,
+                  AppColors.waterColorShade2,
+                ],
+                durations: [
+                  10000,
+                  12000,
+                ],
+                heightPercentages: [val1, val2],
+              ),
+              backgroundColor: AppColors.white,
+              size: const Size(double.infinity, double.infinity),
+              waveAmplitude: 0,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: size.width * 0.05,
+                vertical: size.height * 0.02,
+              ),
+              child: ContainerRow(
+                  title: "Water",
+                  color: AppColors.black,
+                  second: Text(
+                    "💧",
+                    style: AppTextStyles.text18(bold: false, size: size),
+                  )),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  top: size.height * 0.175, left: size.width * 0.05),
+              child: ContainerBottomColumn(
+                title: "liters",
+                value: "1.55",
+                color: AppColors.white,
+              ),
+            ),
+          ]),
         ),
-        child: Stack(children: [
-          WaveWidget(
-            config: CustomConfig(
-              colors: [
-                AppColors.waterColorShade1,
-                AppColors.waterColorShade2,
-              ],
-              durations: [
-                10000,
-                12000,
-              ],
-              heightPercentages: [val1, val2],
-            ),
-            backgroundColor: AppColors.white,
-            size: const Size(double.infinity, double.infinity),
-            waveAmplitude: 0,
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: size.width * 0.05,
-              vertical: size.height * 0.02,
-            ),
-            child: ContainerRow(
-                title: "Water",
-                color: AppColors.black,
-                second: Text(
-                  "💧",
-                  style: AppTextStyles.text18(bold: false, size: size),
-                )),
-          ),
-          Padding(
-            padding: EdgeInsets.only(
-                top: size.height * 0.175, left: size.width * 0.05),
-            child: ContainerBottomColumn(
-              title: "liters",
-              value: "1.55",
-              color: AppColors.white,
-            ),
-          ),
-        ]),
       ),
     );
   }
