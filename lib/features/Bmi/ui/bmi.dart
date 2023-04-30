7638fac1d6469a10f3f404b4de232f1a2989432b
jay-isampelliwar <isampelliwar79@gmail.com>
2023-04-30 15:04:07 +0530

    added

---
+++ lib/features/Bmi/ui/bmi.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:health_tracking_app/core/constants/text_styles.dart';
+import 'package:health_tracking_app/core/helper/router.dart';
+import 'package:health_tracking_app/core/widgets/app_button.dart';
+import 'package:health_tracking_app/core/widgets/app_custom_app_bar.dart';
+import 'package:health_tracking_app/core/widgets/app_text_field.dart';
+import 'package:hive/hive.dart';
+
+import '../../../core/constants/color_constant.dart';
+import '../../../core/widgets/const_size_box.dart';
+
+class Goal extends StatefulWidget {
+  const Goal({super.key});
+
+  @override
+  State<Goal> createState() => _GoalState();
+}
+
+class _GoalState extends State<Goal> {
+  TextEditingController hightTextEditingController = TextEditingController();
+
+  TextEditingController weightTextEditingController = TextEditingController();
+
+  final box = Hive.box("goals");
+
+  @override
+  void dispose() {
+    hightTextEditingController.dispose();
+    weightTextEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(
+            left: size.width * 0.06,
+            right: size.width * 0.06,
+            top: size.height * 0.06,
+            bottom: size.height * 0.04,
+          ),
+          child: Column(
+            children: [
+              CustomAppBar(
+                title: "BMI",
+                subtitle: "Add the details",
+                profile: false,
+              ),
+              AppConstSizeBox.constHightSizedBox(size.height * 0.02),
+              BMIBlock(
+                stepCountTextEditingController: weightTextEditingController,
+                title: "Weight",
+                hindText: "In KG",
+              ),
+              AppConstSizeBox.constHightSizedBox(size.height * 0.01),
+              BMIBlock(
+                stepCountTextEditingController: hightTextEditingController,
+                title: "Height",
+                hindText: "CM",
+              ),
+              AppConstSizeBox.constHightSizedBox(size.height * 0.04),
+              AppButton(
+                child: Text(
+                  "Add",
+                  style: AppTextStyles.text28(bold: true, size: size)
+                      .copyWith(color: AppColors.white),
+                ),
+                onTap: () {
+                  box.put("height", hightTextEditingController);
+                  box.put("weight", weightTextEditingController);
+
+                  routeWithPushReplacement(const Goal(), context);
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BMIBlock extends StatelessWidget {
+  const BMIBlock(
+      {Key? key,
+      required this.stepCountTextEditingController,
+      required this.hindText,
+      required this.title})
+      : super(key: key);
+
+  final TextEditingController stepCountTextEditingController;
+  final String title;
+  final String hindText;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: AppTextStyles.text22(bold: true, size: size),
+        ),
+        AppConstSizeBox.constHightSizedBox(size.height * 0.01),
+        AppTextField(
+          hintText: hindText,
+          inputType: TextInputType.number,
+          obscureText: false,
+          textEditingController: stepCountTextEditingController,
+        )
+      ],
+    );
+  }
+}
