b3083b4cd5fcb1d9f0f72590b8f8df3e847c4539
jay-isampelliwar <isampelliwar79@gmail.com>
2023-05-01 13:36:59 +0530

    uo

--- lib/features/home/widgets/app_dialogBox.dart
+++ lib/features/home/widgets/app_dialogBox.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:health_tracking_app/core/constants/color_constant.dart';
 import 'package:health_tracking_app/core/constants/text_styles.dart';
+import 'package:health_tracking_app/core/widgets/const_size_box.dart';
 import 'package:health_tracking_app/locator.dart';
 import 'package:hive/hive.dart';
 
@@ -12,9 +13,7 @@ Dialog dialogBox({
   required BuildContext context,
 }) {
   final box = Hive.box("localData");
-  final goals = Hive.box("goals");
 
-  int water = box.get("glassWater") ?? 0;
   return Dialog(
     backgroundColor: Colors.transparent,
     child: Container(
@@ -25,85 +24,87 @@ Dialog dialogBox({
       child: Padding(
         padding: EdgeInsets.symmetric(
             vertical: size.height * 0.02, horizontal: size.width * 0.06),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            GestureDetector(
-              onTap: () {
-                locator
-                    .get<HomeBloc>()
-                    .add(HomeWaterDecreaseEvent(water: water));
-              },
-              child: Container(
-                height: size.height * 0.08,
-                width: size.width * 0.2,
-                decoration: BoxDecoration(
-                    color: AppColors.primaryColor, shape: BoxShape.circle),
-                child: Align(
-                  child: Text(
-                    "-",
-                    style: AppTextStyles.text28(bold: false, size: size)
-                        .copyWith(color: AppColors.white),
+        child: SizedBox(
+          height: size.height * 0.2,
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      locator
+                          .get<HomeBloc>()
+                          .add(HomeWaterLevelUpdateEvent(add: false));
+                    },
+                    child: Container(
+                      height: size.height * 0.08,
+                      width: size.width * 0.2,
+                      decoration: BoxDecoration(
+                          color: AppColors.primaryColor,
+                          shape: BoxShape.circle),
+                      child: Align(
+                        child: Text(
+                          "-",
+                          style: AppTextStyles.text28(bold: false, size: size)
+                              .copyWith(color: AppColors.white),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-              ),
-            ),
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  "Number of glass",
-                  style: AppTextStyles.text14(bold: true, size: size),
-                ),
-                BlocBuilder<HomeBloc, HomeState>(
-                  bloc: locator.get<HomeBloc>(),
-                  builder: (context, state) {
-                    if (state is HomeWaterIncreaseState) {
-                      water = state.water;
-                      box.put("glassWater", water);
-
-                      return Text(
-                        state.water.toString(),
-                        style: AppTextStyles.text20(bold: true, size: size),
-                      );
-                    } else if (state is HomeWaterDecreaseState) {
-                      water = state.water;
-                      box.put("glassWater", water);
-
-                      return Text(
-                        state.water.toString(),
-                        style: AppTextStyles.text20(bold: true, size: size),
-                      );
-                    }
-                    return Text(
-                      water.toString(),
-                      style: AppTextStyles.text20(bold: true, size: size),
-                    );
-                  },
-                ),
-              ],
-            ),
-            GestureDetector(
-              onTap: () {
-                locator
-                    .get<HomeBloc>()
-                    .add(HomeWaterIncreaseEvent(water: water));
-              },
-              child: Container(
-                height: size.height * 0.08,
-                width: size.width * 0.2,
-                decoration: BoxDecoration(
-                    color: AppColors.primaryColor, shape: BoxShape.circle),
-                child: Align(
-                  child: Text(
-                    "+",
-                    style: AppTextStyles.text28(bold: false, size: size)
-                        .copyWith(color: AppColors.white),
+                  BlocBuilder<HomeBloc, HomeState>(
+                    bloc: locator.get<HomeBloc>(),
+                    builder: (context, state) {
+                      if (state is HomeWaterLevelUpdate) {
+                        return Text(
+                          state.numberOfGlasses,
+                          style: AppTextStyles.text20(bold: true, size: size),
+                        );
+                      } else {
+                        return Text(
+                          "0",
+                          style: AppTextStyles.text28(bold: true, size: size),
+                        );
+                      }
+                    },
                   ),
-                ),
+                  GestureDetector(
+                    onTap: () {
+                      locator
+                          .get<HomeBloc>()
+                          .add(HomeWaterLevelUpdateEvent(add: true));
+                    },
+                    child: Container(
+                      height: size.height * 0.08,
+                      width: size.width * 0.2,
+                      decoration: BoxDecoration(
+                          color: AppColors.primaryColor,
+                          shape: BoxShape.circle),
+                      child: Align(
+                        child: Text(
+                          "+",
+                          style: AppTextStyles.text28(bold: false, size: size)
+                              .copyWith(color: AppColors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ],
+              AppConstSizeBox.constHightSizedBox(size.height * 0.05),
+              MaterialButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                color: AppColors.primaryColor,
+                child: Text(
+                  "Done",
+                  style: AppTextStyles.text18(bold: false, size: size)
+                      .copyWith(color: AppColors.white),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     ),
