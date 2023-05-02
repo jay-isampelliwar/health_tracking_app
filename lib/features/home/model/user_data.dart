358fd866e91da7f07c34213297b8aeeac31ad5e4
jay-isampelliwar <isampelliwar79@gmail.com>
2023-05-02 11:56:46 +0530

    date and heartrate remove

--- lib/features/home/model/user_data.dart
+++ lib/features/home/model/user_data.dart
@@ -2,18 +2,14 @@ class UserDataModel {
   int step_count;
   int calories_burned;
   int walk_distance;
-  int heart_rate;
   int water;
   int points;
-  String date;
 
   UserDataModel({
     required this.calories_burned,
     required this.walk_distance,
-    required this.heart_rate,
     required this.water,
     required this.points,
-    required this.date,
     required this.step_count,
   });
 }
