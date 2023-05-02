94970e74b94d1b7820e91c78391832edc55d9a65
jay-isampelliwar <isampelliwar79@gmail.com>
2023-05-02 23:14:30 +0530

    added get month fun

--- lib/core/helper/helper.dart
+++ lib/core/helper/helper.dart
@@ -127,4 +127,35 @@ class Helper {
     int totalPoints = stepPoints + caloriePoints;
     return totalPoints + 0.0;
   }
+
+  static String getMonth(int num) {
+    switch (num) {
+      case 1:
+        return 'Jan';
+      case 2:
+        return 'Feb';
+      case 3:
+        return 'Mar';
+      case 4:
+        return 'Apr';
+      case 5:
+        return 'May';
+      case 6:
+        return 'Jun';
+      case 7:
+        return 'Jul';
+      case 8:
+        return 'Aug';
+      case 9:
+        return 'Sep';
+      case 10:
+        return 'Oct';
+      case 11:
+        return 'Nov';
+      case 12:
+        return 'Dec';
+      default:
+        return 'Invalid month number. Please enter a number between 1 and 12.';
+    }
+  }
 }
