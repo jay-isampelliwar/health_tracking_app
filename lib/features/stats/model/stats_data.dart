5035e167af4011302f83bb8000f881bc9ae03094
jay-isampelliwar <isampelliwar79@gmail.com>
2023-05-02 16:27:26 +0530

    null sefty

--- lib/features/stats/model/stats_data.dart
+++ lib/features/stats/model/stats_data.dart
@@ -11,7 +11,7 @@ String dataModelToJson(DataModel data) => json.encode(data.toJson());
 class DataModel {
   bool status;
   String message;
-  List<StatsData> data;
+  List<StatsData?> data;
 
   DataModel({
     required this.status,
@@ -22,66 +22,51 @@ class DataModel {
   factory DataModel.fromJson(Map<String, dynamic> json) => DataModel(
         status: json["status"],
         message: json["message"],
-        data: List<StatsData>.from(
-            json["data"].map((x) => StatsData.fromJson(x))),
+        data:
+            List<StatsData>.from(json["data"].map((x) => StatsData.fromJson(x)))
+                .toList(),
       );
 
   Map<String, dynamic> toJson() => {
         "status": status,
         "message": message,
-        "data": List<dynamic>.from(data.map((x) => x.toJson())),
+        "data": List<dynamic>.from(data.map((x) => x!.toJson())),
       };
 }
 
 class StatsData {
-  String? id;
-  String? userId;
   int? stepCount;
   int? caloriesBurned;
   double? walkDistance;
-  int? heartRate;
   int? water;
   int? points;
   DateTime? date;
-  int? v;
 
   StatsData({
-    this.id,
-    this.userId,
     this.stepCount,
     this.caloriesBurned,
     this.walkDistance,
-    this.heartRate,
     this.water,
     this.points,
     this.date,
-    this.v,
   });
 
-  factory StatsData.fromJson(Map<String, dynamic> json) => StatsData(
-        id: json["_id"],
-        userId: json["user_id"],
+  factory StatsData.fromJson(dynamic json) => StatsData(
         stepCount: json["step_count"],
         caloriesBurned: json["calories_burned"],
         walkDistance: json["walk_distance"]?.toDouble(),
-        heartRate: json["heart_rate"],
         water: json["water"],
         points: json["points"],
         date: DateTime.parse(json["date"]),
-        v: json["__v"],
       );
 
   Map<String, dynamic> toJson() => {
-        "_id": id,
-        "user_id": userId,
         "step_count": stepCount,
         "calories_burned": caloriesBurned,
         "walk_distance": walkDistance,
-        "heart_rate": heartRate,
         "water": water,
         "points": points,
         "date":
             "${date!.year.toString().padLeft(4, '0')}-${date!.month.toString().padLeft(2, '0')}-${date!.day.toString().padLeft(2, '0')}",
-        "__v": v,
       };
 }
