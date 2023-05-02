90c48dcf60966a50c6e9972b66a4fdf3b31571b4
jay-isampelliwar <isampelliwar79@gmail.com>
2023-05-02 10:32:35 +0530

    added return type

--- lib/features/stats/repo/repo.dart
+++ lib/features/stats/repo/repo.dart
@@ -1,9 +1,11 @@
+import 'package:health_tracking_app/features/stats/model/stats_data.dart';
+
 import '../../../api_provider/api_provider.dart';
 
 class StatsRepo {
   final ApiProvider _apiProvider = ApiProvider();
 
-  void getData() async {
-    await _apiProvider.postData();
+  Future<DataModel> getData() async {
+    return await _apiProvider.getData();
   }
 }
