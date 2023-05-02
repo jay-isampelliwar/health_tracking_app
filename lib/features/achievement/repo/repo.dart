975f7cfa6f55aed5fde71ee2b8dd98236bb58566
jay-isampelliwar <isampelliwar79@gmail.com>
2023-05-02 10:30:07 +0530

    added repo return type

--- lib/features/achievement/repo/repo.dart
+++ lib/features/achievement/repo/repo.dart
@@ -1,9 +1,11 @@
+import 'package:health_tracking_app/features/achievement/model/achievement_model.dart';
+
 import '../../../api_provider/api_provider.dart';
 
 class AchievementRepo {
   final ApiProvider _apiProvider = ApiProvider();
 
-  void getAchievement() async {
-    await _apiProvider.getAchievement();
+  Future<AchievementDataModel> getAchievement() async {
+    return await _apiProvider.getAchievement();
   }
 }
